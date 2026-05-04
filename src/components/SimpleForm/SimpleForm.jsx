a79c3742d5719493e45c0d5720268b01142a9ea6
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-05-04 17:24:50 +0600

    learn form e.target

---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,18 @@
+
+const SimpleForm = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.name.value);
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" placeholder="Write your name" />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
