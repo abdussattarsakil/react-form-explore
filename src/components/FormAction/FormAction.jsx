f758d74fa0e5e4c6ba56054afe62e4fb2bf82ff1
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-05-04 17:34:04 +0600

    learn form action and get()

---
+++ src/components/FormAction/FormAction.jsx
@@ -0,0 +1,21 @@
+
+const FormAction = () => {
+
+    const handleFormAction = (formData) => {
+        console.log(formData.get('name'));
+    }
+
+    return (
+        <div>
+            <form action={handleFormAction}>
+                <input type="text" name='name' placeholder="Write Name" />
+                <br />
+                <input type="email" name='email' placeholder="Write Email" />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default FormAction;
